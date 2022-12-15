c9c58ed1802c1222745d7ea6ee9fb81995dfddce
rayala144 <rayalaakhil1999@gmail.com>
2022-12-15 16:11:30 +0530

    General

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { ImageComponent } from './image/image.component';
+import { ButtonComponent } from './button/button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    ImageComponent
+    ImageComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
